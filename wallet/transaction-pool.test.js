7827fe668fdcc1b228b287f3306b857db0c9d73c
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-01-05 01:44:53 +0100

    Added transaction pool

---
+++ wallet/transaction-pool.test.js
@@ -0,0 +1,27 @@
+const TransactionPool = require('./transaction-pool')
+const Transaction = require('./transaction')
+const Wallet = require('./index')
+
+describe('Transaction pool', () => {
+  let tp, transaction, wallet
+
+  beforeEach(() => {
+    tp = new TransactionPool()
+    wallet = new Wallet()
+    transaction = Transaction.newTransaction(wallet, 'r4nd-4dr355', 30)
+    tp.updateOrAddTransaction(transaction)
+  })
+
+  it('adds a transaction to the pool', () => {
+    expect(tp.transactions.find(t => t.id === transaction.id)).toEqual(transaction)
+  })
+
+  it('updates a transaction in the pool', () => {
+    const oldTransaction = JSON.stringify(transaction)
+    // console.log(tp.transactions)
+    const newTransaction = transaction.update(wallet, 'foo-4ddr355', 40)
+    tp.updateOrAddTransaction(newTransaction)
+    // console.log(tp.transactions)
+    expect(JSON.stringify(tp.transactions.find(t => t.id === newTransaction.id))).not.toEqual(oldTransaction)
+  })
+})
