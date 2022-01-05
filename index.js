2e6a10f162ae03b64ea267efaeb978df1aa414c1
Ifeanyichukwuwebdeve <kingifean@gmail.com>
2022-01-05 03:06:17 +0100

    Added get and post to transaction

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 const express = require('express')
+const { redirect } = require('express/lib/response')
 const Blockchain = require('./blockchain')
 const P2pServer = require('./p2p-server')
+const Wallet = require('./wallet')
+const TransactionPool = require('./wallet/transaction-pool')
 
 const PORT = process.env.PORT || 8000
 
 const app = express()
 app.use(express.json())
 const bc = new Blockchain()
+const wallet = new Wallet()
+const tp = new TransactionPool()
 const p2pServer = new P2pServer(bc)
 
 app.get('/blocks', (req, res) => {
@@ -17,6 +22,16 @@ app.post('/mine', (req, res) => {
   const block = bc.addBlock(req.body.data)
   p2pServer.syncChains()
   res.redirect('/blocks')
+})
+
+app.get('/transactions', (req, res) => {
+  res.json(tp.transactions)
+})
+
+app.post('/transact', (req, res) => {
+  const { recipient, amount } = req.body
+  const transaction = wallet.createTransaction(recipient, amount, tp)
+  res.redirect('/transactions')
 })
 
 app.listen(PORT, () => {
